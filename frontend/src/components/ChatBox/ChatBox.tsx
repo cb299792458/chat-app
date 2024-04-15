0e369afc67161bf9e1aa4c4db77170eca6be3583
Brian <brianrlam@gmail.com>
2024-04-15 17:30:25 -0400

    add basic speech input

--- frontend/src/components/ChatBox/ChatBox.tsx
+++ frontend/src/components/ChatBox/ChatBox.tsx
@@ -1,16 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 
 interface ChatBoxProps {
     name: string
 }
 
 const ChatBox: React.FC<ChatBoxProps> = ({name}) => {
+    
     const [messages, setMessages] = React.useState<string[]>([]);
     const [input, setInput] = React.useState<string>("");
 
-    const [talking, setTalking] = React.useState<boolean>(false);
+    const [targetLanguage, setTargetLanguage] = React.useState<string>("es-ES");
+    const handleTargetLanguageChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        setTargetLanguage(e.target.value);
+        SpeechRecognition.stopListening();
+        setTimeout(listenContinuously, 200);
+        // listenContinuously();
+    };
+    const [defaultLanguage, setDefaultLanguage] = React.useState<string>("en-GB");
+    const handleDefaultLanguageChange = (e: React.ChangeEvent<HTMLSelectElement>) => { setDefaultLanguage(e.target.value) };
 
-    React.useEffect(() => {
+    const [talking, setTalking] = React.useState<boolean>(false);
+    
+    React.useEffect(() => {      
         const handleSpaceDown = (e: KeyboardEvent) => {
             if (e.key === " ") setTalking(true);
         };
@@ -34,6 +46,40 @@ const ChatBox: React.FC<ChatBoxProps> = ({name}) => {
         setInput("");
     };
 
+    // const commands = {};
+    const {
+        transcript,
+        interimTranscript,
+        finalTranscript,
+        resetTranscript,
+        listening,
+    } = useSpeechRecognition();
+    
+    const listenContinuously = () => {
+        SpeechRecognition.startListening({
+            continuous: true,
+            language: targetLanguage,
+        });
+    };
+    
+    useEffect(() => {
+        if (finalTranscript) {
+            setMessages([...messages, finalTranscript]);
+            resetTranscript();
+            setInput("");
+        }
+    }, [interimTranscript, finalTranscript]);
+    useEffect(() => {
+        if (transcript) {
+            setInput(transcript);
+        }
+    }, [transcript]);
+        
+    if (!SpeechRecognition.browserSupportsSpeechRecognition()) {
+        console.log('Your browser does not support speech recognition software! Try Chrome desktop, maybe?');
+        return <p>Your browser does not support speech recognition software! Try Chrome desktop, maybe?</p>
+    }
+    
     return (
         <div>
             <h1>ChatBox</h1>
@@ -46,11 +92,40 @@ const ChatBox: React.FC<ChatBoxProps> = ({name}) => {
                 />
                 <button type="submit">Send</button>
             </form>
+
+            I want to speak: 
+            <select onChange={handleTargetLanguageChange} value={targetLanguage}>
+                <option value="en-GB">English</option>
+                <option value="es-ES">Spanish</option>
+                <option value="fr-FR">French</option>
+                <option value="de-DE">German</option>
+                <option value="zh-CN">Mandarin</option>
+                <option value="ja-JP">Japanese</option>
+                <option value="ko-KR">Korean</option>
+            </select>
+            Translate into: 
+            <select onChange={handleDefaultLanguageChange} value={defaultLanguage}>
+                <option value="en-GB">English</option>
+                <option value="es-ES">Spanish</option>
+                <option value="fr-FR">French</option>
+                <option value="de-DE">German</option>
+                <option value="zh-CN">Mandarin</option>
+                <option value="ja-JP">Japanese</option>
+                <option value="ko-KR">Korean</option>
+            </select>
+
             <ul>
                 {messages.map((message, i) => (
                     <li key={i}>{`${name}: ${message}`}</li>
                 ))}
             </ul>
+            {/* <button onClick={resetTranscript}>Reset</button> */}
+            <button onClick={listenContinuously}>Listen</button>
+            <button onClick={SpeechRecognition.stopListening}>Stop</button>
+            {/* <p>Transcript: {transcript}</p>
+            <p>Interim Transcript: {interimTranscript}</p>
+            <p>Final Transcript: {finalTranscript}</p> */}
+            <p>Listening: {listening ? 'Yes' : 'No'}</p>
         </div>
     );
 };
