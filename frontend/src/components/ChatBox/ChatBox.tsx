b3047e512859f06f7b94f370fb3b322287395063
brian <brianrlam@gmail.com>
2024-04-18 16:53:56 -0400

    make basic translation

--- frontend/src/components/ChatBox/ChatBox.tsx
+++ frontend/src/components/ChatBox/ChatBox.tsx
@@ -1,24 +1,81 @@
 import React from "react";
+import axios from "axios";
+import _ from "lodash";
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 
 interface ChatBoxProps {
     name: string
 }
+enum Language {
+    American_English = 'en-US',
+    Spanish = 'es-ES',
+    French = 'fr-FR',
+    German = 'de-DE',
+    Mandarin_Chinese = 'zh-CN',
+    Cantonese_Chinese = 'yue',
+    Traditional_Chinese = 'zh-tw',
+    Japanese = 'ja-JP',
+    Korean = 'ko-KR',
+    Thai = 'th-TH',
+    Vietnamese = 'vi-VN',
+    Russian = 'ru-RU',
+    Greek = 'el-GR',
+}
+const languageNames: { [key: string]: string } = {};
+Object.keys(Language).forEach((key) => {
+    const value = Language[key as keyof typeof Language];
+    languageNames[value] = key;
+});
+interface Message {
+    fromUser: boolean;
+    source: Language;
+    target: Language;
+    text: string;
+    translation: string;
+}
+
+const apiKey: string | undefined = process.env.REACT_APP_GOOGLE_CLOUD_API_KEY;
 
 const ChatBox: React.FC<ChatBoxProps> = ({name}) => {
     
-    const [messages, setMessages] = React.useState<string[]>([]);
+    const [messages, setMessages] = React.useState<Message[]>([{
+        fromUser: false,
+        source: Language.American_English,
+        target: Language.American_English,
+        text: "Hi! I'm Nina. You can talk to me to practice your language skills!",
+        translation: "Hi! I'm Nina. You can talk to me to practice your language skills!",
+    }]);
     const [input, setInput] = React.useState<string>("");
 
-    const [targetLanguage, setTargetLanguage] = React.useState<string>("es-ES");
-    const [defaultLanguage, setDefaultLanguage] = React.useState<string>("en-US");
-    const handleTargetLanguageChange = (e: React.ChangeEvent<HTMLSelectElement>) => setTargetLanguage(e.target.value);
-    const handleDefaultLanguageChange = (e: React.ChangeEvent<HTMLSelectElement>) => setDefaultLanguage(e.target.value);
-       
-    const sendMessage = (e: React.FormEvent<HTMLFormElement>) => {
+    const [practiceLanguage, setPracticeLanguage] = React.useState<string>("es-ES");
+    const [preferredLanguage, setPreferredLanguage] = React.useState<string>("en-US");
+    const handlePracticeLanguageChange = (e: React.ChangeEvent<HTMLSelectElement>) => setPracticeLanguage(e.target.value);
+    const handlePreferredLanguageChange = (e: React.ChangeEvent<HTMLSelectElement>) => setPreferredLanguage(e.target.value);
+
+    const addMessage = async (text: string) => {
+        const body = {
+            q: text,
+            source: practiceLanguage,
+            target: preferredLanguage,
+        };
+
+        const res = await axios.post(`https://translation.googleapis.com/language/translate/v2?key=${apiKey}`, body);
+        const translation = res.data.data.translations[0].translatedText;
+
+        const newMessage: Message = {
+            fromUser: true,
+            source: practiceLanguage as Language,
+            target: preferredLanguage as Language,
+            text,
+            translation,
+        };
+        setMessages([...messages, newMessage]);
+    }
+
+    const sendMessage = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         if (listening) return;
-        setMessages([...messages, input]);
+        await addMessage(input);
         setInput("");
     };
 
@@ -34,23 +91,24 @@ const ChatBox: React.FC<ChatBoxProps> = ({name}) => {
     const listenContinuously = () => {
         SpeechRecognition.startListening({
             continuous: true,
-            language: targetLanguage,
+            language: practiceLanguage,
         });
     };
 
-    React.useEffect(listenContinuously, [targetLanguage]);
+    React.useEffect(listenContinuously, [practiceLanguage]);
     React.useEffect(() => {
-        if (transcript) {
+        if (transcript && listening) {
             setInput(transcript);
         }
-    }, [transcript]);
+    }, [transcript, listening]);
     React.useEffect(() => {
         if (finalTranscript) {
-            setMessages([...messages, finalTranscript]);
+            addMessage(finalTranscript);
             resetTranscript();
             setInput("");
         }
-    }, [interimTranscript, finalTranscript, messages, resetTranscript]);
+    // eslint-disable-next-line
+    }, [interimTranscript, finalTranscript, messages]);
 
     const handleToggleMode = () => {
         setInput("");
@@ -65,22 +123,6 @@ const ChatBox: React.FC<ChatBoxProps> = ({name}) => {
         console.log('Your browser does not support speech recognition software! Try Chrome desktop, maybe?');
         return <h1>Your browser does not support speech recognition software! Try Chrome desktop, maybe?</h1>
     }
-
-    const languages: { [key: string]: string }  = {
-        'en-US': 'American English',
-        'en-GB': 'British English',
-        'es-ES': 'Spanish (Spain)',
-        'fr-FR': 'French',
-        'de-DE': 'German',
-        'zh-CN': 'Mandarin Chinese',
-        'yue': 'Cantonese Chinese',
-        'zh-tw': 'Traditional Chinese',
-        'ja-JP': 'Japanese',
-        'ko-KR': 'Korean',
-        'th-TH': 'Thai',
-        'vi-VN': 'Vietnamese',
-        'ru-RU': 'Russian',
-    }
     
     return (
         <div>
@@ -89,9 +131,9 @@ const ChatBox: React.FC<ChatBoxProps> = ({name}) => {
                 <input
                     type="text"
                     value={input}
-                    onChange={(e) => setInput(e.target.value)}
+                    onChange={(e) => !listening && setInput(e.target.value)}
                 />
-                <button type="submit">{listening ? 'This is what I\'ve heard!' : 'Send your Written Message!'}</button>
+                <button type="submit">{listening ? '<- This is what I\'ve heard!' : '-> Send your typed message!'}</button>
             </form>
 
             <span>
@@ -105,21 +147,35 @@ const ChatBox: React.FC<ChatBoxProps> = ({name}) => {
 
             <br/>
             I want to practice my:{" "}
-            <select onChange={handleTargetLanguageChange} value={targetLanguage}>
-                {Object.entries(languages).map(([code, lang]) => <option value={code} key={code}>{lang}</option>)}
+            <select onChange={handlePracticeLanguageChange} value={practiceLanguage}>
+                {Object.entries(Language).map(([name, code]) => <option value={code} key={code}>{name.replace('_', " ")}</option>)}
             </select>
 
             <br/>
             Translate for me into:{" "} 
-            <select onChange={handleDefaultLanguageChange} value={defaultLanguage}>
-                {Object.entries(languages).map(([code, lang]) => <option value={code} key={code}>{lang}</option>)}
+            <select onChange={handlePreferredLanguageChange} value={preferredLanguage}>
+                {Object.entries(Language).map(([name, code]) => <option value={code} key={code}>{name.replace('_', " ")}</option>)}
             </select>
 
-            <ul>
-                {messages.map((message, i) => (
-                    <li key={i}>{`${name}: ${message}`}</li>
-                ))}
-            </ul>
+            <table>
+                <thead>
+                    <tr>
+                        <th>From</th>
+                        <th>Text</th>
+                        <th>Translation</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {messages.map((message, i) => (
+                        <tr key={i}>
+                            <td>{message.fromUser ? name : "Nina"}</td>
+                            <td>{_.unescape(message.text)}</td>
+                            <td>{_.unescape(message.translation)}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+
         </div>
     );
 };
