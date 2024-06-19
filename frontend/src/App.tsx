e73ddc519239eaff0ee7fa6f16ab5493fa3b9428
Brian Lam <brianrlam@gmail.com>
2024-06-19 10:17:31 -0400

    add posthog demo

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
 import ChatBox from './components/ChatBox';
+import PostHogComponent from './components/PostHogComponent';
 
 function App() {
     return (
         <div className="App">
             <ChatBox />
+            <PostHogComponent />
         </div>
     );
 }
